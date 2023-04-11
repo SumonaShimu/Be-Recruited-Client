397569f170474a4bfbc4b9a96f7dcd31f72545e0
Sumona Akter Shimu <sumona59shimu@gmail.com>
2023-04-11 12:27:06 +0600

    assignment statistics added

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='flex justify-between bg-gray-900 text-white p-10' >
-            <h6>@2023 CareerHub. All Rights Reserved</h6>
-            <h6>Powered by CareerHub</h6>
+        <div className='md:flex justify-between bg-gray-900 text-white p-10' >
+            <h6>@2023 BeRecruited. All Rights Reserved</h6>
+            <h6>Powered by BeRecruited</h6>
         </div>
     );
 };
--- src/component/Statistics/Statistics.jsx
+++ src/component/Statistics/Statistics.jsx
@@ -1,11 +1,181 @@
 import React from 'react';
+import {
+    Radar,
+    RadarChart,
+    PolarGrid,
+    Legend,
+    PolarAngleAxis,
+    PolarRadiusAxis
+} from "recharts";
 
 const Statistics = () => {
+
+    const data = [
+        {
+            subject: "Assignment-1",
+            optainedMark: 60,
+            fullMark: 60
+        },
+        {
+            subject: "Assignment-2",
+            optainedMark: 60,
+            fullMark: 60
+        },
+        {
+            subject: "Assignment-3",
+            optainedMark: 60,
+            fullMark: 60
+        },
+        {
+            subject: "Assignment-4",
+            optainedMark: 60,
+            fullMark: 60
+        },
+        {
+            subject: "Assignment-5",
+            optainedMark: 60,
+            fullMark: 60
+        },
+        {
+            subject: "Assignment-6",
+            optainedMark: 60,
+            fullMark: 60
+        },
+        {
+            subject: "Assignment-7",
+            optainedMark: 60,
+            fullMark: 60
+        },
+        {
+            subject: "Assignment-8",
+            optainedMark: 60,
+            fullMark: 60
+        }
+    ];
     return (
-        <div className='min-h-[80vh]'>
-            all assignment result here
+        <div className='w-[100%] h-[100vh]'>
+            <RadarChart
+            cx="50%" cy="50%"
+            outerRadius='80%'
+            width={800}
+            height={600}
+            data={data}
+            className='mx-auto'
+        >
+            <PolarGrid />
+            <PolarAngleAxis dataKey="subject" />
+            <PolarRadiusAxis domain={[0, 60]} />
+            <Radar
+                name="Optained Mark"
+                dataKey="optainedMark:"
+                stroke="#8884d8"
+                fill="#8884d8"
+                fillOpacity={0.3}
+            />
+            <Radar
+                name="Total Marks"
+                dataKey="fullMark"
+                stroke="#82ca9d"
+                fill="#82ca9d"
+                fillOpacity={0.5}
+            />
+            <Legend />
+        </RadarChart>
         </div>
+        
     );
 };
 
 export default Statistics;
+
+
+
+
+/**
+import "./styles.css";
+import React from "react";
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  Legend,
+  PolarAngleAxis,
+  PolarRadiusAxis
+} from "recharts";
+
+const data = [
+  {
+    subject: "Assignment-1",
+    optainedMark: 60,
+    fullMark: 60
+  },
+  {
+    subject: "Assignment-2",
+    optainedMark: 60,
+    fullMark: 60
+  },
+  {
+    subject: "Assignment-3",
+    optainedMark: 60,
+    fullMark: 60
+  },
+  {
+    subject: "Assignment-4",
+    optainedMark: 60,
+    fullMark: 60
+  },
+  {
+    subject: "Assignment-5",
+    optainedMark: 60,
+    fullMark: 60
+  },
+  {
+    subject: "Assignment-6",
+    optainedMark: 60,
+    fullMark: 60
+  },
+  {
+    subject: "Assignment-7",
+    optainedMark: 60,
+    fullMark: 60
+  },
+  {
+    subject: "Assignment-8",
+    optainedMark: 60,
+    fullMark: 60
+  }
+];
+
+export default function App() {
+  return (
+    <RadarChart
+      cx={300}
+      cy={250}
+      outerRadius={150}
+      width={500}
+      height={500}
+      data={data}
+    >
+      <PolarGrid />
+      <PolarAngleAxis dataKey="subject" />
+      <PolarRadiusAxis angle={45} domain={[0, 60]} />
+      <Radar
+        name="optained Mark:"
+        dataKey="optainedMark:"
+        stroke="#8884d8"
+        fill="#8884d8"
+        fillOpacity={0.3}
+      />
+      <Radar
+        name="TotalMarks"
+        dataKey="fullMark"
+        stroke="#82ca9d"
+        fill="#82ca9d"
+        fillOpacity={0.3}
+      />
+      <Legend />
+    </RadarChart>
+  );
+}
+
+ */
